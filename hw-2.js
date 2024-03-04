06796dd59c0a51e9836e7a2865883c2e17638687
Albert Akhmetov <albahm74@gmail.com>
2024-03-05 00:01:11 +0300

    making corrections

--- hw-2.js
+++ hw-2.js
@@ -1,66 +1,66 @@
 //__task_1__
-// let a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
+ let num = 10;
+ alert(num);
+ num = 20;
+ alert(num);
 
 //__task_2__
-// const releaseYearFirstIphone = 2007;
-// alert(`Год выпуска первого Iphone: ${releaseYearFirstIphone}`);
+ const releaseYearFirstIphone = 2007;
+ alert(`Год выпуска первого Iphone: ${releaseYearFirstIphone}`);
 
 //__task_3__
-// const nameCreatorJavaScript = "Брендан Эйх";
-// alert(`Имя Создателя JavaScript: ${nameCreatorJavaScript}`);
+ const nameCreatorJavaScript = "Брендан Эйх";
+ alert(`Имя Создателя JavaScript: ${nameCreatorJavaScript}`);
 
 //__task_4__
-// let a = 10;
-// let b = 2;
-// alert(Числа: ${a}; ${b}.\nСумма: ${a + b}\nРазность: ${a - b}\nПроизведение: ${a * b}\nЧастное: ${a / b}\n `);
+ let a = 10;
+ let b = 2;
+ alert(`Числа: ${a}; ${b}.\nСумма: ${a + b}\nРазность: ${a - b}\nПроизведение: ${a * b}\nЧастное: ${a / b}\n `);
 
 //__task_5__
-// let a = 2;
-// a **= 5;
-// alert(a);
+ let c = 2;
+ c **= 5;
+ alert(c);
 
 //__task_6__
-// let a = 9;
-// let b = 2;
-// alert(`Остаток от деления: ${a % b}`);
+ let d = 9;
+ let e = 2;
+ alert(`Остаток от деления: ${d % e}`);
 
 //__task_7__
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num++;
-// num--;
-// alert(num);
+ let numb = 1;
+ numb += 5;
+ numb -= 3;
+ numb *= 7;
+ numb /= 3;
+ numb++;
+ numb--;
+ alert(numb);
 
 //__task_8__
-// let age = number(prompt("Сколько вам лет?"));
-// alert(age);
+ let age = number(prompt("Сколько вам лет?"));
+ alert(age);
 
 //__task_9__
-// let user = {
-//     name: 'Frank',
-//     age: 45,
-//     isAdmin: true
-// };
+ let user = {
+     name: 'Frank',
+     age: 45,
+     isAdmin: true
+    };
 
 //__task_9.1__
-// user['city of residence'] = "Mannheim";
+ user['city of residence'] = "Mannheim";
 
 //__task_9.2__
-// user.age = 50;
+ user.age = 50;
 
 //__task_9.3__
-// delete user['city of residence'];
+ delete user['city of residence'];
 
 //__task_9.4__
-// let info = prompt("Какую информацию хотите узнать о пользователе?");
-// alert(user[info]);
+ let info = prompt("Какую информацию хотите узнать о пользователе?");
+ alert(user[info]);
 
 //__task_10__
-// let userName = prompt(`Введите ваше имя: `);
-// alert(`Привет, ${userName}`);
+ let userName = prompt(`Введите ваше имя: `);
+ alert(`Привет, ${userName}`);
