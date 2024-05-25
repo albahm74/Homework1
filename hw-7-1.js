bc2a2a4e7f35b68209a80c13212796d81708f0fe
Albert Akhmetov <albahm74@gmail.com>
2024-05-25 22:31:08 +0300

    corrections made

--- hw-7-1.js
+++ hw-7-1.js
@@ -1,3 +1,10 @@
 let fruit =['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
 fruit = fruit.sort(() => Math.random() - 0.5);
-//console.log(fruit);
+alert(fruit);
+let oneAnswer = prompt('Введите первый элемент массива');
+let twoAnswer = prompt('Введите последний элемент массива');
+if (oneAnswer === fruit[0] && twoAnswer === fruit[fruit.length - 1]) {
+    alert("Вы ввели оба слова правильно.");
+} else {
+    alert("Вы ввели одно или оба слова неправильно, попобуйте всё сначала.");
+}
--- hw-7.js
+++ hw-7.js
@@ -66,3 +66,10 @@ console.log(formDate(new Date()));
 let fruit =['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
 fruit = fruit.sort(() => Math.random() - 0.5);
 console.log(fruit);
+let oneAnswer = prompt('Введите первый элемент массива');
+let twoAnswer = prompt('Введите последний элемент массива');
+if (oneAnswer === fruit[0] && twoAnswer === fruit[fruit.length - 1]) {
+    alert("Вы ввели оба слова правильно.");
+} else {
+    alert("Вы ввели одно или оба слова неправильно, попобуйте всё сначала.");
+}
